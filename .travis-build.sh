d84d1707563be7172281baf76a4511c2f3015c0a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2019-05-01 13:54:00 +0200

    .travis-build.sh: make -j3

--- .travis-build.sh
+++ .travis-build.sh
@@ -6,7 +6,7 @@ case x$DOCKER in
     x)
 	mkdir -p build
 	cd build
-	../configure --srcdir=.. && CAT_CHECKDIR_ON_ERROR=t make distcheck
+	../configure --srcdir=.. && CAT_CHECKDIR_ON_ERROR=t make -j3 distcheck
 	;;
     *i386*)
 	docker run -i -v "${PWD}:/src" $DOCKER /bin/bash -c "linux32 --32bit i386 /src/.docker-build.sh"
